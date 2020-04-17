d0661f87d6a8d3743cfef131cd961d62bb08e53f
张晓檬 <921996026@qq.com>
2020-04-18 00:54:03 +0800

    创建项目

---
+++ android/app/src/main/java/com/happy/dream/dream_catcher/MainActivity.java
@@ -0,0 +1,13 @@
+package com.happy.dream.dream_catcher;
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity;
+import io.flutter.embedding.engine.FlutterEngine;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine) {
+    GeneratedPluginRegistrant.registerWith(flutterEngine);
+  }
+}
